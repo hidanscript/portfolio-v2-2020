0f95a5a26f552a0ce2db347a1f4170558a168ad1
hidanscript <dmarcano@gmail.com>
2020-05-06 18:07:47 -0300

    Fixes en opera

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -7,7 +7,7 @@ function Navbar() {
   let github_url = "https://github.com/hidanscript"
 
   return(
-    <nav>
+    <nav className="prueba-nav">
       <ul>
         <li><AnchorLink className="anchor" href="#portfolio">PORTFOLIO</AnchorLink></li>
         <li><a className="anchor" href={github_url} target="_blank" rel="noopener noreferrer">GITHUB</a></li>
